4b514838450e670bb767850a4aee724ef1a09585
Salvo Falcone <salvofalcone57@gmail.com>
2023-06-21 22:41:56 +0200

    added camera filters

--- src/components/Camera/Camera.jsx
+++ src/components/Camera/Camera.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { filtersData } from "../../mocks/filters";
 import "./index.css";
 
 const Camera = () => {
@@ -6,6 +7,8 @@ const Camera = () => {
     audio: false,
     video: { width: 600, height: 1400 },
   });
+
+  const [filterState, setFilterState] = useState("");
 
   useEffect(() => {
     navigator.mediaDevices
@@ -23,7 +26,26 @@ const Camera = () => {
       });
   }, []);
 
-  return <video className="Camera"></video>;
+  const onSetFilterState = (state) => setFilterState(state);
+
+  return (
+    <div className="Camera__Main">
+      <video className={`Camera ${filterState}`}></video>
+
+      <div className="Camera__Filters__Container">
+        <ul className="Camera__Filters">
+          {filtersData?.map((filter) => (
+            <li
+              className={filter.name}
+              onClick={() => onSetFilterState(filter.name)}>
+              <img src="https://picsum.photos/100" alt="preview filter" className="Camera__Filter__Preview"/>
+              <p className="Camera__Filter__Name">{filter.title}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Camera;
---
+++ src/mocks/filters.js
@@ -0,0 +1,22 @@
+export const filtersData = [
+  {
+    id: 1,
+    name: "claredon",
+    title: "claredon",
+  },
+  {
+    id: 2,
+    name: "sepia",
+    title: "sepia",
+  },
+  {
+    id: 3,
+    name: "moon",
+    title: "moon",
+  },
+  {
+    id: 4,
+    name: "lark",
+    title: "lark",
+  },
+];
